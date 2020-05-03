d729bf940337cfa0e2943e2ac5aec98515fa3f1b
Kei178 <keisuke.inaba.cpa@gmail.com>
2020-05-03 18:32:25 +1000

    Add CableReady to StimulusReflex

--- app/reflexes/messages_reflex.rb
+++ app/reflexes/messages_reflex.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 class MessagesReflex < ApplicationReflex
+  include CableReady::Broadcaster
+
   def like
     message = Message.find(element.dataset[:id])
     message.increment! :like_count
+    cable_ready['message'].text_content(
+      selector: "#message-#{message.id}-likes",
+      text: message.like_count
+    )
+    cable_ready.broadcast
   end
 end
